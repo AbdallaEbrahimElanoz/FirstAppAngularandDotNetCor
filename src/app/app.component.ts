9f14fe3b5bd5b24962924fa75d253abe64abc649
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-06-23 23:20:57 +0300

    Directives & Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  colors=[' Red','Green','purple','Yellow','White','Black']
+  colors = [' Red', 'Green', 'purple', 'Yellow', 'White', 'Black'];
   title = 'FirstAppAngularandDotNetCor';
-  name='Abdullah Elanouz';
-  Twoway='';
-  Age=23;
-  source='https://th.bing.com/th/id/OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF?pid=ImgDet&rs=1';
-  color:any;
+  name = 'Abdullah Elanouz';
+  Twoway = '';
+  Age = 23;
+  source =
+    'https://th.bing.com/th/id/OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF?pid=ImgDet&rs=1';
+  color: any;
+  isAuth: boolean = false;
+  logSate: string = 'Login';
+
+  toggle() {
+    this.isAuth = !this.isAuth;
+    this.logSate = this.isAuth ? 'logout' : 'Login';
+  }
 
   myFunction() {
-    alert("Hello");
+    alert('Hello');
     console.log('hello');
   }
-  changeColor(color:any=this.colors) {
-
-    console.log(this.colors=[color]);
-
+  changeColor(color: any = this.colors) {
+    console.log((this.colors = [color]));
   }
 }
