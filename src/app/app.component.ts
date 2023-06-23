3103e573df4623767b16a50042c5cd75450b2fa5
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-06-23 20:40:55 +0300

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  colors=[' Red','Green','purple','Yellow','White','Black']
   title = 'FirstAppAngularandDotNetCor';
   name='Abdullah Elanouz';
   Age=23;
   source='https://th.bing.com/th/id/OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF?pid=ImgDet&rs=1';
+  color:any;
+
+  myFunction() {
+    alert("Hello");
+    console.log('hello');
+  }
+  changeColor(color:any=this.colors) {
+
+    console.log(this.colors=[color]);
+
+  }
 }
